3f426d140f9a4d0f81f3fc358757f3939a5df9d5
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-30 23:54:37 -0400

    created post route in notesRoutes file

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -3,15 +3,15 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.status(200).send("you got 20 notes");
+    res.status(200).send("You just fetched the notes");
+});
+
+router.post("/", (req, res) => {
+    res.status(201).json({message:"Note created successfully!"})
 });
 
 export default router;
 
-
-app.post("/api/notes", (req, res) => {
-    res.status(201).json({message:"Note created successfully!"})
-});
 
 app.put("/api/notes/:id", (req, res) => {
     res.status(200).json({message:"Note updated successfully!"})
