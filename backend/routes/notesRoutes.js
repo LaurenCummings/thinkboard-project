41f248b273b3b029b5d8367e1d3b14a231c41c3d
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-30 23:53:30 -0400

    created get route in notesRoutes file

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -2,4 +2,21 @@ import express from "express";
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    res.status(200).send("you got 20 notes");
+});
+
 export default router;
+
+
+app.post("/api/notes", (req, res) => {
+    res.status(201).json({message:"Note created successfully!"})
+});
+
+app.put("/api/notes/:id", (req, res) => {
+    res.status(200).json({message:"Note updated successfully!"})
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    res.status(200).json({message:"Note deleted successfully!"})
+});
