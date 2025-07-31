db5402798755ee92db56b8dd3003a3af93fd5989
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-30 23:56:50 -0400

    created delete route in notesRoutes file

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -14,9 +14,8 @@ router.put("/:id", (req, res) => {
     res.status(200).json({message:"Note updated successfully!"})
 });
 
-export default router;
-
-
-app.delete("/api/notes/:id", (req, res) => {
+router.delete("/:id", (req, res) => {
     res.status(200).json({message:"Note deleted successfully!"})
 });
+
+export default router;
