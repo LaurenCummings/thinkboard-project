ba869e5716ea6dfad75864109a34bc6e6a8a0871
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-30 23:37:20 -0400

    created post request route

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ app.get("/api/notes", (req, res) => {
     res.status(200).send("you got 20 notes");
 });
 
+app.post("/api/notes", (req, res) => {
+    res.status(201).json({message:"post created successfully!"})
+})
+
 
 app.listen(5001, () => {
     console.log("Server started on PORT: 5001");
