a55fc3565ef4bf9167c9065a1634fa3f69bd2123
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-30 23:51:20 -0400

    added use method to redirect server to notesRoutes file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
 
 const app = express();
+
+app.use("/api/notes", notesRoutes);
 
 app.get("/api/notes", (req, res) => {
     res.status(200).send("you got 20 notes");
