4a2ec47253b23a5b73e90a907fd46ed3a6112c40
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-31 23:29:29 -0400

    added delete controller to controller file

--- backend/controllers/notesController.js
+++ backend/controllers/notesController.js
@@ -9,3 +9,7 @@ export function createNote(req, res) {
 export function updateNote(req, res) {
     res.status(200).json({message:"Note updated successfully!"})
 };
+
+export function deleteNote(req, res) {
+    res.status(200).json({message:"Note deleted successfully!"})
+};
