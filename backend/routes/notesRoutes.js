3f5de93364d7ab7f5635a0bb6820cbf7ec0bb780
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-31 23:28:44 -0400

    deleted controller from route file and imported controller from controller file

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllNotes, createNote } from "../controllers/notesController.js";
+import { getAllNotes, createNote, updateNote } from "../controllers/notesController.js";
 
 const router = express.Router();
 
@@ -7,9 +7,7 @@ router.get("/", getAllNotes);
 
 router.post("/", createNote);
 
-router.put("/:id", (req, res) => {
-    res.status(200).json({message:"Note updated successfully!"})
-});
+router.put("/:id", updateNote);
 
 router.delete("/:id", (req, res) => {
     res.status(200).json({message:"Note deleted successfully!"})
