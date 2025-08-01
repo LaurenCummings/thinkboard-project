acd6d90e154ee0fe1d3ae6c1b6587f32a4ae833b
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-31 23:19:38 -0400

    added extension to import

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllNotes } from "../controllers/notesController";
+import { getAllNotes } from "../controllers/notesController.js";
 
 const router = express.Router();
 
