9a629b2749c7cec5185f1ece336ebef125a8b43c
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-31 23:31:55 -0400

    formatting change

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -4,11 +4,8 @@ import { getAllNotes, createNote, updateNote, deleteNote } from "../controllers/
 const router = express.Router();
 
 router.get("/", getAllNotes);
-
 router.post("/", createNote);
-
 router.put("/:id", updateNote);
-
 router.delete("/:id", deleteNote);
 
 export default router;
