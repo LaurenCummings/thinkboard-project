564b84639c15d698112060582d1f00000b485dcc
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-01 23:29:47 -0400

    added catch block and export to db file

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
+export const connectDB = async () => {
     try {
         await mongoose.connect("mongodb+srv://laurencummings7:IaH6mBAbQIink9yg@cluster0.yddm3b0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-        console.log("MONGODB CONNECTED");
+        console.log("MONGODB CONNECTED SUCCESSFULLY!");
     } catch(error) {
-
+        console.error("Error connecting to MONGODB", error);
     }
 }
