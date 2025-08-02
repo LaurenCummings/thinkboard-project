e9ffa32e631ebd21427dfb9cc9b983ee84f1f4c0
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-01 23:27:44 -0400

    added connection string to db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-
+        await mongoose.connect("mongodb+srv://laurencummings7:IaH6mBAbQIink9yg@cluster0.yddm3b0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        console.log("MONGODB CONNECTED");
     } catch(error) {
 
     }
