e2abeeb9852ef679b5986423d6a81f640c17c684
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-01 23:47:30 -0400

    added name for database

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://laurencummings7:IaH6mBAbQIink9yg@cluster0.yddm3b0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect("mongodb+srv://laurencummings7:IaH6mBAbQIink9yg@cluster0.yddm3b0.mongodb.net/notes_db?retryWrites=true&w=majority&appName=Cluster0")
         console.log("MONGODB CONNECTED SUCCESSFULLY!");
     } catch(error) {
         console.error("Error connecting to MONGODB", error);
