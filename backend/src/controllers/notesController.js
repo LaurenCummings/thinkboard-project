bce6ce057070e427661f09b19007c696d0092683
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-04 23:19:44 -0400

    added message and status code if note to delete is not found

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -40,5 +40,5 @@ export async function updateNote(req, res) {
 
 export async function deleteNote(req, res) {
     const deletedNote = await Note.findByIdAndDelete(req.params.id);
-
+    if (!deletedNote) return res.status(404).json({ message: "Note not found" });
 };
