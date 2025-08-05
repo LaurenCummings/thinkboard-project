0775ebc667e5554d7a14eb899772541ae2d1e4f2
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-04 22:40:16 -0400

    added param to set the new note to the updated note, not the original note

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -26,8 +26,10 @@ export async function createNote(req, res) {
 export async function updateNote(req, res) {
     try {
         const { title, content } = req.body;
-        const updatedNote = await Note.findByIdAndUpdate(req.params.id, { title, content });
-        if (!updatedNote) return res.status(404).json({ message: "Note not found" })
+        const updatedNote = await Note.findByIdAndUpdate(req.params.id, { title, content },{
+            new:true
+        });
+        if (!updatedNote) return res.status(404).json({ message: "Note not found" });
 
         res.status(200).json({ message: "Note updated successfully" })
     } catch (error) {
