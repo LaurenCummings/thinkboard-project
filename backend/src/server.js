d148162eb22e2c5d535608e0505f2a3631274550
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-06 23:31:44 -0400

    added call to rateLimiter from server

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ connectDB();
 
 // middleware
 app.use(express.json());
+app.use(rateLimiter);
 
 // app.use((req, res, next) => {
 //     console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
