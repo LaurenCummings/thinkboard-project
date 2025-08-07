71ed3b3e61e1035bd9b5dcc678df441f9ed678ac
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-06 23:40:33 -0400

    added code to check for too many requests from user and return status code and message if true

--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -3,9 +3,16 @@ import ratelimit from "../config/upstash.js";
 const rateLimiter = async (req, res, next) => {
 
     try {
+        const { success } = await ratelimit.limit("my-limit-key");
+
+        if (!success) {
+            return res.status(429).json({
+                message: "Too many requests, please try again later"
+            });
+        }
 
     } catch (error) {
-        
+
     }
 }
 
