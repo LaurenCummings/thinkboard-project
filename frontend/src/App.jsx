37bcbb33c1c2858ce249f4d39ef3a4f8c581de5c
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-09 23:10:29 -0400

    added route for createPage and imported

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
-import Homepage from "./pages/Homepage";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
       </Routes>
     </div>
   )
