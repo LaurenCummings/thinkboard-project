287a7f994592343d4402af7d908fdde857553de3
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-10 21:50:50 -0400

    added tailwind test classes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import toast from "react-hot-toast";
 const App = () => {
   return (
     <div>
-      <button onClick={() => toast.error("congrats")}>Click me</button>
+      <button onClick={() => toast.error("congrats")} className="text-red-500 p-4">Click me</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
