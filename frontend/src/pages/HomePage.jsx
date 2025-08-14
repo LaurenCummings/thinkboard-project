778678c6a8caae38d18648d5cf56a310641b09e6
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-13 23:52:49 -0400

    replaced note mapping with notecard component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -44,9 +44,7 @@ function HomePage() {
         { notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             { notes.map((note) => (
-              <div>
-                {note.title} | {note.content}
-              </div>
+              <NoteCard key={note._id} note={note} />
             ))}
           </div>
         )}
