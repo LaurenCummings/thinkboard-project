2466582725bf5bcdabd3cca2efdc6ccfd653a679
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-13 23:59:56 -0400

    added title to notecard link

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -3,7 +3,9 @@ import { Link } from "react-router";
 function NoteCard({ note }) {
   return (
     <Link to={`/note/${note._id}`} className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#00FF9D]">
-        
+        <div className="card-body">
+            <h3 className="card-title text-base-content">{note.title}</h3>
+        </div>
     </Link>
   )
 }
