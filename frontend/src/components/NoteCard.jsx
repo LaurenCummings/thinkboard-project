26a6f2e0a4b3be10c2c9a66baf3ecdb732a2b401
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-14 00:02:53 -0400

    added note createdAt printout

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -6,6 +6,12 @@ function NoteCard({ note }) {
         <div className="card-body">
             <h3 className="card-title text-base-content">{note.title}</h3>
             <p className="text-base-content/70 line-clamp-3">{note.cotent}</p>
+            <div className="card-actions justify-between items-center mt-4">
+                <span>
+                    {note.createdAt}
+                </span>
+
+            </div>
         </div>
     </Link>
   )
