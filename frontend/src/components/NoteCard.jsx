3039bb6968b501c88814209902119e805d966a45
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-14 00:08:28 -0400

    added more styling to trash can icon

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -13,7 +13,7 @@ function NoteCard({ note }) {
                 </span>
                 <div className="flex items-center gap-1">
                     <PenSquareIcon className="size-4" />
-                    <button className="btn btn-ghost">
+                    <button className="btn btn-ghost btn-xs text-error">
                         <Trash2Icon className="size-4" />
                     </button>
                 </div>
