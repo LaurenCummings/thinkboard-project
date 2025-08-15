e856eea675f01c0da6281b5328651fab91ceeb72
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-14 23:58:48 -0400

    added link back to home page

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router";
 
 function CreatePage() {
   const [title, setTitle] = useState("");
@@ -13,7 +14,9 @@ function CreatePage() {
     <div className="min-h-screen bg-base-200">
       <div className="container mx-auto px-4 py-8">
         <div className="max-w-2xl mx-auto">
-
+          <Link to={"/"} className="btn btn-ghost mb-6">
+            
+          </Link>
         </div>
       </div>
     </div>
