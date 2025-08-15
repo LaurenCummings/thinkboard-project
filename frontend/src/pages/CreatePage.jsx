03e0da0610d9dd78cc768be214b646a29344fe2d
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-15 00:00:45 -0400

    added back to notes text inside link back to home page

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -17,6 +17,7 @@ function CreatePage() {
         <div className="max-w-2xl mx-auto">
           <Link to={"/"} className="btn btn-ghost mb-6">
             <ArrowLeftIcon className="size-5" />
+            Back to Notes
           </Link>
         </div>
       </div>
