683558a2d969837ecfd2d14768632b44ca79ee2e
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-15 00:37:01 -0400

    added textarea for content

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -39,6 +39,12 @@ function CreatePage() {
                   <label className="label">
                     <span className="label-text">Content</span>
                   </label>
+                  <textarea
+                    placeholder="Write your note here..."
+                    className="textarea textarea-bordered h-32"
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                  />
                 </div>
               </form>
             </div>
