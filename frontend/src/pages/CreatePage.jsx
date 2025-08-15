a47648449c9675c7842311a94252b1ed141ad7e2
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-15 00:07:10 -0400

    created newNote form element

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -22,6 +22,11 @@ function CreatePage() {
           <div className="card bg-base-100">
             <div className="card-body">
               <h2 className="card-title text-2xl mb-4">Create New Note</h2>
+              <form onSubmit={handleSubmit}>
+                <div>
+                  
+                </div>
+              </form>
             </div>
           </div>
         </div>
