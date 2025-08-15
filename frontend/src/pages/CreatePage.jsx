de4a09c726187514536992680b2efcb1f611dcc7
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-14 23:56:21 -0400

    added some styling to createPage

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -6,11 +6,15 @@ function CreatePage() {
   const [loading, setLoading] = useState(false);
 
   function handleSubmit() {
-    
+
   }
 
   return (
-    <div>CreatePage</div>
+    <div className="min-h-screen bg-base-200">
+      <div className="container mx-auto px-4 py-8">
+
+      </div>
+    </div>
   )
 }
 
