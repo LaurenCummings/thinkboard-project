65515a3274eae38db16b3e23c9334f001bd343f7
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-16 00:21:12 -0400

    imported axios api config into homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import RateLimitedUI from "../components/RateLimitedUI";
 import NoteCard from "../components/NoteCard";
-import axios from "axios";
+import api from "../lib/axios";
 import toast from "react-hot-toast";
 
 function HomePage() {
