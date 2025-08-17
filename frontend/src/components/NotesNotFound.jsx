ec2ae64bd6452eee8fd172ed506721454cbb355a
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-17 00:21:09 -0400

    added heading to no notes component

--- frontend/src/components/NotesNotFound.jsx
+++ frontend/src/components/NotesNotFound.jsx
@@ -4,9 +4,10 @@ import { Link } from "react-router";
 function NotesNotFound() {
     return (
         <div className="flex flex-col items-center justify-center py-16 space-y-6 max-w-md mx-auto text-center">
-            <div>
+            <div className="bg-primary/10 rounded-full p-8">
                 <NotebookIcon />
             </div>
+            <h3 className="text-2xl font-bold">No notes yet</h3>
         </div>
     )
 }
