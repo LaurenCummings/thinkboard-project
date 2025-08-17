1040e5d5e66d71dbb1c797078a7298da1fda9611
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-17 00:19:39 -0400

    added notebookicon and some styling to notesNotFound component

--- frontend/src/components/NotesNotFound.jsx
+++ frontend/src/components/NotesNotFound.jsx
@@ -3,7 +3,11 @@ import { Link } from "react-router";
 
 function NotesNotFound() {
     return (
-        <div>NotesNotFound</div>
+        <div className="flex flex-col items-center justify-center py-16 space-y-6 max-w-md mx-auto text-center">
+            <div>
+                <NotebookIcon />
+            </div>
+        </div>
     )
 }
 
