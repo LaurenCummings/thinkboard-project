129aa4d5911572e0aa3ab4cece6b7a7c3dc2b651
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-18 00:12:02 -0400

    added some content to note detail page

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -40,9 +40,13 @@ function NoteDetailPage() {
   }
 
   return (
-    <div>
-      <div>
-
+    <div className="min-h-screen bg-base-200">
+      <div className="container mx-auto px-4 py-8">
+        <div className="flex items-center justify-between mb-6">
+          <Link to="/" className="btn btn-ghost">
+          
+          </Link>
+        </div>
       </div>
     </div>
   )
