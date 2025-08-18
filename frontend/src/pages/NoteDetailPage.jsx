fd2f6a8a7086a499512a5aaa500f7b8e290093ea
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-17 23:30:12 -0400

    created state constants for notedetailpage

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 
 function NoteDetailPage() {
+  const [note, setNote] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [saving, setSaving] = useState(false);
+
   return (
     <div>NoteDetailPage</div>
   )
