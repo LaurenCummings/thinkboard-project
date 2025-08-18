adcf620dc99507af30873648213b8ed00c9b62b2
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-17 23:33:29 -0400

    created navigate constant using useNavigate hook

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -6,6 +6,8 @@ function NoteDetailPage() {
   const [loading, setLoading] = useState(true);
   const [saving, setSaving] = useState(false);
 
+  const navigate = useNavigate();
+
   return (
     <div>NoteDetailPage</div>
   )
