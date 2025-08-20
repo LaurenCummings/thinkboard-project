c39ff49c4646be2d8bef0b145d3de02146312311
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-20 00:06:55 -0400

    added conditional to check if we're in production that will serve up the frontend as a static resource if we are

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,11 +27,13 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-})
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
