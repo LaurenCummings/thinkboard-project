52b3049b5dd51c753baf17224685ab431f46a6fa
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-19 23:55:51 -0400

    added constant for directory

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // middleware
 app.use(cors({
@@ -25,6 +26,8 @@ app.use(rateLimiter);
 // });
 
 app.use("/api/notes", notesRoutes);
+
+app.use(express.static(path.join()))
 
 connectDB().then(() => {
     app.listen(PORT, () => {
