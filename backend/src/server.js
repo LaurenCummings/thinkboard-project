a654b5ea5f842e06709cca46b850be7c10e6faf3
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-20 00:15:17 -0400

    added conditional to only run cors middleware if we're not in production

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,9 +13,12 @@ const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
 // middleware
-app.use(cors({
-    origin: "http://localhost:5173",
-}));
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }));   
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
