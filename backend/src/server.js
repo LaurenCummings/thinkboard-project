8d806baa8d244a6aed06e90f6e69ac2a6349d195
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-08-19 23:51:59 -0400

    imported path package into backend server file

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
