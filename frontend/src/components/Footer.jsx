7dd2b74d8deb827c03b83234c724662bc12fd859
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-16 23:57:37 -0400

    imported footer to homepage

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,15 @@
 const Footer = () => {
-  return <div className="text-red-500">Footer</div>;
+  return (
+    <div className="text-red-500 m-10">
+      <a target="_blank" href="https://icons8.com/icon/9_NQcM4E-KeP/pencil">
+        Pencil
+      </a>{" "}
+      icon by{" "}
+      <a target="_blank" href="https://icons8.com">
+        Icons8
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import RateLimitedUI from "../components/RateLimitedUI";
 import NoteCard from "../components/NoteCard";
 import NotesNotFound from "../components/NotesNotFound";
@@ -12,8 +13,8 @@ function HomePage() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchNotes = async() => {
-      try{
+    const fetchNotes = async () => {
+      try {
         const res = await api.get("/notes");
         console.log(res.data);
         setNotes(res.data);
@@ -29,7 +30,7 @@ function HomePage() {
       } finally {
         setLoading(false);
       }
-    }
+    };
     fetchNotes();
   }, []);
 
@@ -37,23 +38,25 @@ function HomePage() {
     <div className="min-h-screen">
       <Navbar />
 
-      { isRateLimited && <RateLimitedUI /> }
+      {isRateLimited && <RateLimitedUI />}
 
       <div className="max-w-7xl mx-auto p-4 mt-6">
-        { loading && <div className="text-center text-primary py-10">Loading notes...</div> }
+        {loading && (
+          <div className="text-center text-primary py-10">Loading notes...</div>
+        )}
 
-        { notes.length === 0 && !isRateLimited && <NotesNotFound /> }
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
 
-        { notes.length > 0 && !isRateLimited && (
+        {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            { notes.map((note) => (
+            {notes.map((note) => (
               <NoteCard key={note._id} note={note} setNotes={setNotes} />
             ))}
           </div>
         )}
       </div>
     </div>
-  )
+  );
 }
 
 export default HomePage;
